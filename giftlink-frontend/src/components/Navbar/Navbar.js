758abaa8009abb5673d34d600c29d006832231cf
gnavarroo1 <g.navarroo@hotmail.com>
2025-11-10 16:01:14 -0500

    implementacion de vista de busqueda

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -9,10 +9,14 @@ export default function Navbar() {
                 <ul className="navbar-nav">
                     {/* Task 1: Add links to Home and Gifts below*/}
                     <li className="nav-item">
-                        <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
+                        <a className="nav-link" href="/">Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/app">Gifts</a> {/* Updated link to MainPage */}
+                        <a className="nav-link" href="/app">Gifts</a>
+                    </li>
+                    {/* Add Search link to navigate to SearchPage */}
+                    <li className="nav-item">
+                        <a className="nav-link" href="/app/search">Search</a>
                     </li>
                 </ul>
             </div>
